4a37b89639fca13f574c62d63526a3327b9140fa
Kenneth Rawlings <krawlings@alianza.com>
2016-03-27 11:47:51 -0600

    Updating hello

--- src/02-hello/hello.go
+++ src/02-hello/hello.go
@@ -2,10 +2,13 @@
 package main
 
 // fmt contains methods for formatting data in strings and io
+// https://golang.org/pkg/fmt/
 import "fmt"
 
 // Not that there is no return value or arguments to our main method. We will get to that later.
 func main() {
     // A function with a capital letter?!?! What is that about? Yeah, we'll get to that later as well.
-    fmt.Print("Hello, gopher!")
+    fmt.Println("Hello,", "gopher!") // Arguments will be converted to strings and concatenated together with spaces in between
+    fmt.Printf("Goodbye, %s.\n", "gopher") // Does what you would think.
 }
+// BTW, where are all the semicolons.
