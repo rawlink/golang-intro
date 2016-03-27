9f1e04d4c56c61ec916615f993e3670db84bf7a7
Kenneth Rawlings <krawlings@alianza.com>
2016-03-27 11:17:29 -0600

    Just 'go'ing to be friendly

---
+++ src/02-hello/hello.go
@@ -0,0 +1,11 @@
+// All files containing a main method must be in the main package
+package main
+
+// fmt contains methods for formatting data in strings and io
+import "fmt"
+
+// Not that there is no return value or arguments to our main method. We will get to that later.
+func main() {
+    // A function with a capital letter?!?! What is that about? Yeah, we'll get to that later as well.
+    fmt.Print("Hello, gopher!")
+}
