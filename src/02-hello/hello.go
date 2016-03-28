6e6f39c5632ac3735ff339c1f833feda337bb771
Kenneth Rawlings <krawlings@alianza.com>
2016-03-27 20:20:17 -0600

    Adding types and vars. Updating hello

--- src/02-hello/hello.go
+++ src/02-hello/hello.go
@@ -4,14 +4,20 @@ package main
 // fmt contains methods for formatting data in strings and io
 // https://golang.org/pkg/fmt/
 import "fmt"
+// unused imports will not compile
+//import "math"
 
-// Not that there is no return value or arguments to our main method. We will get to that later.
+// Note the lack of a return value or arguments on the main method. More on that later.
 func main() {
     // A function with a capital letter?!?! What is that about? Yeah, we'll get to that later as well.
     fmt.Println("Hello,", "gopher!") // Arguments will be converted to strings and concatenated together with spaces in between
     fmt.Printf("Goodbye, %s.\n", "gopher") // Does what you would think.
-    // BTW, where are all the semicolons?
+
+    // Unused variables will not compile
+    //var i int = 0
 }
+
+// BTW, where are all the semicolons? Semicolons are only used in Go when they are needed -- which is not at the end of a line of code
 
 // Let's try some things at the command line
 // go build
